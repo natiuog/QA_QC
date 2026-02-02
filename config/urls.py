27ed29f608261afedd39d09bab948ff1261bcf61
natiuog <natigelemso2015@gmail.com>
2026-02-02 22:27:21 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('telegram/', include('telegram_bot.urls')),
+    path('', include('telegram_bot.urls')),
 ]
 
 # Serve media files during development
